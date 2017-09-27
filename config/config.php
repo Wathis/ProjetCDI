34c7a892c79c0fa79c3eb4f48cb6cdf89cd2246b
Mathis Delaunay <delaunaymathis@yahoo.fr>
2017-09-27 13:35:55 +0200

    Test

--- config/config.php
+++ config/config.php
@@ -15,6 +15,7 @@ define('URL_PROTOCOL', '//');
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
 define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
+
 /**
  * Configuration de la base de donnée
 */
